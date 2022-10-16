225c4bc64546c51c6bbdc861cc92fe2c132541c2
jepManalo <jeffreypmanalo@gmail.com>
2022-10-16 16:13:55 +0200

    Eclipse connecting to Appium Server
    - Successfully updated codes to connect to Appium Server by changing the URL address

--- AppiumPayback/lib/src/main/java/StepDefinitions/CommonSteps.java
+++ AppiumPayback/lib/src/main/java/StepDefinitions/CommonSteps.java
@@ -1,52 +1,52 @@
-package StepDefinitions;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import io.cucumber.java.en.*;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
-
-import org.testng.Assert;
-import 	org.testng.annotations.*;
-
-public class CommonSteps {
-
-	AndroidDriver driver = null;
-
-	@Given("I open PayBack application in android")
-	public void IOpenPayBackApplicationInAndroid() throws MalformedURLException {
-
-		DesiredCapabilities capability = new DesiredCapabilities();
-		capability.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-		capability.setCapability("platformName", "Android");
-		capability.setCapability("appPackage", "de.payback.client.android");
-		capability.setCapability("appActivity", "de.payback.app.deeplinks.StarterActivity");
-
-		driver = new AndroidDriver(new URL("https://127.0.0.1:4723/wd/hub"), capability);
-
-	}
-
-	@When("I complete action")
-	public void ICompleteAction() {
-
-	}
-
-	@Then("I validate the outcomes")
-	public void IValidateTheOutcomes() {
-
-		String abc = "abc";
-		String def = "def";
-		
-		Assert.assertEquals(abc, def);
-
-	}
-
-	@And("I close the driver")
-	public void ICloseTheDriver() {
-
-	}
-
-}
+//package StepDefinitions;
+//
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//
+//import io.cucumber.java.en.*;
+//import io.appium.java_client.android.AndroidDriver;
+//import io.appium.java_client.remote.MobileCapabilityType;
+//
+//import org.testng.Assert;
+//import 	org.testng.annotations.*;
+//
+//public class CommonSteps {
+//
+//	AndroidDriver driver = null;
+//
+//	@Given("I open PayBack application in android")
+//	public void IOpenPayBackApplicationInAndroid() throws MalformedURLException {
+//
+//		DesiredCapabilities capability = new DesiredCapabilities();
+//		capability.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
+//		capability.setCapability("platformName", "Android");
+//		capability.setCapability("appPackage", "de.payback.client.android");
+//		capability.setCapability("appActivity", "de.payback.app.deeplinks.StarterActivity");
+//
+//		driver = new AndroidDriver(new URL("https://127.0.0.1:4723/wd/hub"), capability);
+//
+//	}
+//
+//	@When("I complete action")
+//	public void ICompleteAction() {
+//
+//	}
+//
+//	@Then("I validate the outcomes")
+//	public void IValidateTheOutcomes() {
+//
+//		String abc = "abc";
+//		String def = "def";
+//		
+//		Assert.assertEquals(abc, def);
+//
+//	}
+//
+//	@And("I close the driver")
+//	public void ICloseTheDriver() {
+//
+//	}
+//
+//}
--- AppiumPayback/lib/src/test/java/tests/BaseClass.java
+++ AppiumPayback/lib/src/test/java/tests/BaseClass.java
@@ -3,7 +3,6 @@ package tests;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 
@@ -17,19 +16,27 @@ public class BaseClass {
 	@BeforeTest
 	public void Setup () throws MalformedURLException {
 		
-		DesiredCapabilities capability = new DesiredCapabilities();
-		capability.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-		capability.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		capability.setCapability(MobileCapabilityType.PLATFORM_VERSION, "");
-		capability.setCapability(MobileCapabilityType.UDID, "");
-		capability.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
-		capability.setCapability(MobileCapabilityType.APP, "");
-		capability.setCapability("appPackage", "de.payback.client.android");
-		capability.setCapability("appActivity", "de.payback.app.deeplinks.StarterActivity");
-
-		driver = new AndroidDriver(new URL("https://127.0.0.1:4723/wd/hub"), capability);
-	}
-	
+		try {			
+			DesiredCapabilities capability = new DesiredCapabilities();
+			
+			capability.setCapability(MobileCapabilityType.DEVICE_NAME, "device");
+			capability.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+			capability.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
+			capability.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+			capability.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
+			capability.setCapability("appPackage", "de.payback.client.android");
+			capability.setCapability("appActivity", "de.payback.app.deeplinks.StarterActivity");
+			
+			URL url = new URL("http://localhost:4723/wd/hub"); //using 0.0.0.0
+			
+			driver = new AndroidDriver(url, capability);
+		}
+		catch(Exception e){
+			System.out.println("Cause is " + e.getCause());
+			System.out.println("Message is " + e.getMessage());
+			e.printStackTrace();
+		}
+	}	
 	
 	@AfterTest
 	public void TearDown() {
